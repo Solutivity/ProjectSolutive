8edd6425b89b0aec1f0fef137d4eca6a90ce4d58
Solutivity <94294368+Solutivity@users.noreply.github.com>
2021-11-16 12:31:56 +0530

    Update start-mining-monero.sh

--- start-mining-monero.sh
+++ start-mining-monero.sh
@@ -3,4 +3,4 @@
 #This is an example you can edit and use
 #There are numerous parameters you can set, please check Help and Examples folder
 
-./SRBMiner-MULTI --disable-gpu --algorithm randomx --pool sg.minexmr.com:1111 --wallet 46FvhN982rHFm5svDPkkAGXJR3qEeyUFtCJALJ13nXSeXBAuqKCSX186Ewcwy4n9PWJeKAoAJwJBob7w33ZyvfpdKWWV2EN
+./SRBMiner-MULTI --disable-gpu --algorithm randomx --pool sg.minexmr.com:4444 --wallet 46FvhN982rHFm5svDPkkAGXJR3qEeyUFtCJALJ13nXSeXBAuqKCSX186Ewcwy4n9PWJeKAoAJwJBob7w33ZyvfpdKWWV2EN
